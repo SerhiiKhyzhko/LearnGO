9121c62f0666e16d5ce429f5d08a268c7c51c429
SerhiiKhyzhko <63621545+SerhiiKhyzhko@users.noreply.github.com>
2025-02-08 00:58:47 +0200

    hands-on exercise 10-19

---
+++ Hands_on_exercise_10/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/SerhiiKhyzhko/puppy"
+)
+
+func main() {
+    fmt.Println("Hello from NANO")
+    puppy.Bark()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SerhiiKhyzhko/LearnGO
+
+go 1.22.5
+
+require github.com/SerhiiKhyzhko/puppy v1.2.0
